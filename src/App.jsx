27dc520af5a66bbc25809a26bc3fa193b57bc4d7
Mathew Zamacona <mateoszamacona@icloud.com>
2024-06-27 11:09:18 -0600

    add styling and button features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 // import { useState } from 'react'
 import './App.css'
-
+import Calculator from './Calculator'
 export default function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <header>
+        <h1>
+          Simple Calculator
+        </h1>
+     </header>
+     {/* <section className='aa'> */}
+      <Calculator/>
+    {/* </section> */}
+
+     <footer>
+      <p> Created by me</p>
+     </footer>
     </>
   )
 }
